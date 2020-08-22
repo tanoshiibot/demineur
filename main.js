c7ea63d6e1f315739451c9dd5f206b5052c90d35
tanoshiibot <olivia.fantinel@gmail.com>
2020-08-22 14:38:24 +0200

    Lorsque le message dépasse la limite de caractère que peut contenir un message discord, plutôt que d'essayer et de ne rien envoyer du tout, le bot envoie un message indiquant que le nombre de caractères est trop grand.

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const bot = new (require("discord.js").Client)();
 const minesweeper = require("./minesweeper.js");
 
-
 const PREFIX = ">"
 
 bot.on("ready", _ => console.log("Bot démarré"));
@@ -15,16 +14,33 @@ bot.on("message", message => {
 
         const [, ...args] = message.content.substring(PREFIX.length).split(/\s+/);
 
-        console.log(args[0]);
+        //change the value of defaultBombs to change the default number of bombs
+        const defaultBombs = 16;
+        const defaultEmoji = "\u{1F4A3}";
+        //change the value of defaultEmoji to change the default emoji
 
-        if (args && (args[0] < 0 || args[0] > 64 || (isNaN(args[0]) && args[0] !== undefined))){
+        function sendMinesweeper (howManybombs, emoji, positions){
+            const messageMinesweeper = `${howManyBombs} ${emoji}\n${positions}`;
+            if (messageMinesweeper.length > 2000){
+            return message.channel.send("Too many characters (>2000). Don't put too much bombs if your emoji name is long.")
+            } else {
+            return message.channel.send(messageMinesweeper);
+            }
+        }
+
+        if (args.length == 1 && (isNaN(args[0]) && args[0] !== undefined)){
+            let bombs = 16
+            let emoji = args[0] 
+            const { howManyBombs, positions } = minesweeper(bombs, emoji); 
+            sendMinesweeper(howManyBombs, emoji, positions);
+        } else if (args[0] < 0 || args[0] > 64 || (isNaN(args[0]) && args[0] !== undefined)){
             return message.channel.send("Choose a valid amount of bombs (between 0 and 64)");
         }
 
-        const bombs = + args[0] || 16;
-        const emoji = args[1] || "\u{1F4A3}";
-        const { howManyBombs, positions } = minesweeper(bombs, emoji);            
-        message.channel.send(`${howManyBombs} ${emoji}\n${positions}`);
+        const bombs = + args[0] || defaultBombs;
+        const emoji = args[1] || defaultEmoji;
+        const { howManyBombs, positions } = minesweeper(bombs, emoji);
+        sendMinesweeper(howManyBombs, emoji, positions);
     }
 });
 
